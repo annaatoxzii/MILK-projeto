032b0185d4afe13d5619dfee06e19cbc7d65cc07
annaatoxzii <annaatoxzii@gmail.com>
2025-09-10 14:59:49 -0300

    TESTE

---
+++ javascript/header.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const menuToggle = document.getElementById('menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+
+    if (menuToggle && mainNav) {
+        menuToggle.addEventListener('click', () => {
+            menuToggle.classList.toggle('active');
+            mainNav.classList.toggle('active');
+        });
+    }
+
+});
