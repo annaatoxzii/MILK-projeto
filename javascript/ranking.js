a6840d753e7396b72c1d079a73d9e734d42c4085
annaatoxzii <annaatoxzii@gmail.com>
2025-09-17 13:58:41 -0300

    commit ranking

---
+++ javascript/ranking.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const ranking = document.querySelector(".ranking");
+  const colEsquerda = document.querySelector(".coluna2");
+  const colDireita  = document.querySelector(".coluna3");
+  const colCentro   = document.querySelector(".coluna1");
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        [colDireita, colEsquerda, colCentro].forEach(c => c.classList.remove("show"));
+
+        setTimeout(() => colDireita.classList.add("show"), 0);
+        setTimeout(() => colEsquerda.classList.add("show"), 700);
+        setTimeout(() => colCentro.classList.add("show"), 1400);
+
+        obs.disconnect();
+      }
+    });
+  }, { threshold: 0.15 }); 
+
+  observer.observe(ranking);
+});
